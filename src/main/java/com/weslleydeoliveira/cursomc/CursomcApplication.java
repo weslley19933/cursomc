ecd2756a4b81b295340e6e991f70762f540324ac
weslley19933 <weslley.oli93@gmail.com>
2021-02-08 12:35:19 -0300

    Criado a classe Prouto e a sua associação de muitos para muito com a entidade Categoria

--- src/main/java/com/weslleydeoliveira/cursomc/CursomcApplication.java
+++ src/main/java/com/weslleydeoliveira/cursomc/CursomcApplication.java
@@ -8,13 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.weslleydeoliveira.cursomc.domain.Categoria;
+import com.weslleydeoliveira.cursomc.domain.Produto;
 import com.weslleydeoliveira.cursomc.repositories.CategoriaRepository;
+import com.weslleydeoliveira.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
-	CategoriaRepository categoriaRepository;
+	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -26,8 +30,22 @@ public class CursomcApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null, "Informática" );
 		Categoria cat2 = new Categoria(null, "Escritório" );
 		
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
 		
+		
+		//Exemplo para vincular determinados produtos a determinadas categorias e vice versa
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		//Exemplo para vincular determinados produtos a determinadas categorias e vice versa
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 
 }
