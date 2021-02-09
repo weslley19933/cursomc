c7e7dbad242cc0a91d7330d6cff472b5629bc330
weslley19933 <weslley.oli93@gmail.com>
2021-02-09 17:21:37 -0300

    Endopoint /clientes/{id} disponível

---
+++ src/main/java/com/weslleydeoliveira/cursomc/services/ClienteService.java
@@ -0,0 +1,27 @@
+package com.weslleydeoliveira.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.weslleydeoliveira.cursomc.domain.Cliente;
+import com.weslleydeoliveira.cursomc.repositories.ClienteRepository;
+import com.weslleydeoliveira.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired //auto instancia o Objeto
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + "; Tipo: " + Cliente.class.getName()));
+	}
+	
+	
+	
+	
+}
