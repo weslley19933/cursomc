bc0f3675d859701d72deb239d5b110224f9b2323
weslley19933 <weslley.oli93@gmail.com>
2021-02-09 22:15:35 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/weslleydeoliveira/cursomc/services/PedidoService.java
@@ -0,0 +1,27 @@
+package com.weslleydeoliveira.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.weslleydeoliveira.cursomc.domain.Pedido;
+import com.weslleydeoliveira.cursomc.repositories.PedidoRepository;
+import com.weslleydeoliveira.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired //auto instancia o Objeto
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + "; Tipo: " + Pedido.class.getName()));
+	}
+	
+	
+	
+	
+}
